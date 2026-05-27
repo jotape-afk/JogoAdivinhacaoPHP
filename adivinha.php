01fb8897e2489c0d7475f0c1c8cc7d0af94fc743
jotape <joaopeu72@gmail.com>
2026-05-27 04:12:24 -0300

    adiciona numeros aleatorios para a senha e melhora legibilidade e feedback visual no terminal

--- adivinha.php
+++ adivinha.php
@@ -1,10 +1,10 @@
 <?php
 
-$senha = 10;
+$senha = rand(1, 100);
 
 do{
 
-    $num = (int) readline("Digite um numero: ");
+    $num = (int) readline("Digite um numero (1 - 100): ");
 
     $dif = abs($senha - $num);
     $dif1 = $senha - $num;
@@ -12,13 +12,13 @@ do{
     if    ($dif1 > 0) {echo("↑ ");}
     elseif($dif1 < 0) {echo("↓ ");}
 
-    if    ($dif <= 3) {echo("QUEIMANDO" . PHP_EOL);}
-    elseif($dif <= 6) {echo("Bem quente" . PHP_EOL);}
-    elseif($dif <= 9) {echo("Esquentando" . PHP_EOL);}
-    else              {echo("Muito frio" . PHP_EOL);}
+    if    ($dif <= 3) {echo("QUEIMANDO" . PHP_EOL . PHP_EOL);}
+    elseif($dif <= 6) {echo("Bem quente" . PHP_EOL . PHP_EOL);}
+    elseif($dif <= 9) {echo("Esquentando" . PHP_EOL . PHP_EOL);}
+    else              {echo("Muito frio" . PHP_EOL . PHP_EOL);}
 
 }while($num != $senha);
 
-echo("CERTA RESPOSTA");
+echo(PHP_EOL . "CERTA RESPOSTA: " . $senha);
 
 ?>
