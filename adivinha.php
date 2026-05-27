884ba1042238f2649ad146860cc08cc9b9bebed1
jotape <joaopeu72@gmail.com>
2026-05-27 04:09:49 -0300

    adiciona dica indicando se o numero é maior ou menor

--- adivinha.php
+++ adivinha.php
@@ -3,8 +3,14 @@
 $senha = 10;
 
 do{
+
     $num = (int) readline("Digite um numero: ");
+
     $dif = abs($senha - $num);
+    $dif1 = $senha - $num;
+
+    if    ($dif1 > 0) {echo("↑ ");}
+    elseif($dif1 < 0) {echo("↓ ");}
 
     if    ($dif <= 3) {echo("QUEIMANDO" . PHP_EOL);}
     elseif($dif <= 6) {echo("Bem quente" . PHP_EOL);}
